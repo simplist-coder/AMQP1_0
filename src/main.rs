638bc9e12da1da88588f4dbabe2f2789213d0b59
Simplist-Coder <simplist.coder@gmail.com>
2023-08-22 18:48:48 +0200

    created some basic modules

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
+use anyhow::Result;
+use std::net::SocketAddr;
+use tokio::net::{TcpListener, TcpStream};
+
+mod connection;
+mod error;
+mod frame;
+mod link;
+mod performative;
+mod session;
 mod types;
 
-fn main() {
-    println!("Hello, world!");
-    println!("again");
+#[tokio::main]
+async fn main() -> Result<()> {
+    let addr = "127.0.0.1:8080".parse::<SocketAddr>()?;
+    let stream = TcpListener::bind(&addr).await?;
+
+    loop {
+        let (socket, _) = stream.accept().await?;
+    }
+    Ok(())
 }
---
+++ src/performative.rs
@@ -0,0 +1,11 @@
+pub enum Performative {
+    Open,
+    Begin,
+    Attach,
+    Flow,
+    Transfer,
+    Disposition,
+    Detach,
+    End,
+    Close,
+}
