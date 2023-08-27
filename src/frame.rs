63c25cc8573c6b756731c15ea4f9be42d51b5a9f
Simplist-Coder <simplist.coder@gmail.com>
2023-08-27 12:00:25 +0200

    added some more unit tests

--- src/frame.rs
+++ src/frame.rs
@@ -79,7 +79,7 @@ impl TryFrom<&[u8]> for FrameType {
 mod tests {
     use crate::{error::AppError, frame::FrameType};
 
-    use super::Header;
+    use super::*;
 
     fn create_header_bytes(size: u32, doff: u8, frame_type: u8) -> Vec<u8> {
         let mut res = vec![];
@@ -111,5 +111,35 @@ mod tests {
     }
 
     #[test]
-    fn header_deserialization_should_fail_on_malformed_header() {}
+    fn header_deserialization_should_fail_on_malformed_header() {
+        let data = create_header_bytes(1, 0, 0);
+        let header = Header::try_from(data.as_slice());
+        assert!(header.is_err());
+        assert_eq!(
+            header.err().unwrap(),
+            AppError::MalformedFrame("".to_string())
+        );
+    }
+
+    #[test]
+    fn frame_is_malformed_when_size_is_smaller_than_required_frame_header_size() {
+        let data = create_header_bytes(7, 0, 0);
+        let frame = Frame::try_from(data.as_slice());
+        assert!(frame.is_err());
+        assert_eq!(
+            frame.err().unwrap(),
+            AppError::MalformedFrame("".to_string())
+        );
+    }
+
+    #[test]
+    fn frame_is_malformed_if_doff_is_smaller_than_2() {
+        let data = create_header_bytes(20, 1, 0);
+        let frame = Frame::try_from(data.as_slice());
+        assert!(frame.is_err());
+        assert_eq!(
+            frame.err().unwrap(),
+            AppError::MalformedFrame("".to_string())
+        );
+    }
 }
