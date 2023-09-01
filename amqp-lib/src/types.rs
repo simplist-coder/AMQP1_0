baf1bac93ba1979cbab7c8a419637b3699e50263
Simplist-Coder <simplist.coder@gmail.com>
2023-09-01 18:45:45 +0200

    added from impls for all types, added wrapper types, defined trait for type construction

--- amqp-lib/src/types.rs
+++ amqp-lib/src/types.rs
@@ -1,3 +1,16 @@
+
+
+pub trait Constructable {
+    fn constructor(&self) -> Constructor;
+}
+pub struct Timestamp(u64);
+pub struct Binary();
+pub struct Symbol();
+pub struct List();
+pub struct Map();
+pub struct Array();
+pub struct Uuid();
+pub struct Constructor(u8);
 pub enum PrimitiveType {
     Null,
     Boolean(bool),
@@ -14,15 +27,49 @@ pub enum PrimitiveType {
     Decimal32(f32),
     Decimal64(f64),
     Char(char),
-    Timestamp(u64),
-    Uuid(String),
-    Binary,
+    Timestamp(Timestamp),
+    Uuid(Uuid),
+    Binary(Binary),
     String(String),
-    Symbol,
-    List,
-    Map,
-    Array,
+    Symbol(Symbol),
+    List(List),
+    Map(Map),
+    Array(Array),
+
+    // Decimal128(), Not supported yet
 }
+
+impl Constructable for PrimitiveType {
+
+    fn constructor(&self) -> Constructor {
+        match self {
+            Self::Null=> Constructor(0x40),
+            Self::Boolean(b)=> Constructor(0x56),
+            Self::Ubyte(_)=> Constructor(0x50),
+            Self::Ushort(_)=> Constructor(0x40),
+            Self::Uint(_)=> Constructor(0x40),
+            Self::Ulong(_)=> Constructor(0x40),
+            Self::Byte(_)=> Constructor(0x40),
+            Self::Short(_)=> Constructor(0x40),
+            Self::Int(_)=> Constructor(0x40),
+            Self::Long(_)=> Constructor(0x40),
+            Self::Float(_)=> Constructor(0x40),
+            Self::Double(_)=> Constructor(0x40),
+            Self::Decimal32(_)=> Constructor(0x40),
+            Self::Decimal64(_)=> Constructor(0x40),
+            Self::Char(_)=> Constructor(0x40),
+            Self::Timestamp(_)=> Constructor(0x40),
+            Self::Uuid(_)=> Constructor(0x40),
+            Self::Binary(_)=> Constructor(0x40),
+            Self::String(_)=> Constructor(0x40),
+            Self::Symbol(_)=> Constructor(0x40),
+            Self::List(_)=> Constructor(0x40),
+            Self::Map(_)=> Constructor(0x40),
+            Self::Array(_)=> Constructor(0x40),
+        }
+    }
+}
+
 
 impl From<bool> for PrimitiveType {
     fn from(value: bool) -> Self {
@@ -90,3 +137,56 @@ impl From<f64> for PrimitiveType {
     }
 }
 
+impl From<char> for PrimitiveType {
+    fn from(value: char) -> Self {
+        PrimitiveType::Char(value)
+    }
+}
+
+impl From<Timestamp> for PrimitiveType {
+    fn from(value: Timestamp) -> Self {
+        PrimitiveType::Timestamp(value)
+    }
+}
+
+impl From<Uuid> for PrimitiveType {
+    fn from(value: Uuid) -> Self {
+        PrimitiveType::Uuid(value)
+    }
+}
+
+impl From<Binary> for PrimitiveType {
+    fn from(value: Binary) -> Self {
+        PrimitiveType::Binary(value)
+    }
+}
+
+impl From<String> for PrimitiveType {
+    fn from(value: String) -> Self {
+        PrimitiveType::String(value)
+    }
+}
+
+impl From<Symbol> for PrimitiveType {
+    fn from(value: Symbol) -> Self {
+        PrimitiveType::Symbol(value)
+    }
+}
+
+impl From<List> for PrimitiveType {
+    fn from(value: List) -> Self {
+        PrimitiveType::List(value)
+    }
+}
+
+impl From<Map> for PrimitiveType {
+    fn from(value: Map) -> Self {
+        PrimitiveType::Map(value)
+    }
+}
+
+impl From<Array> for PrimitiveType {
+    fn from(value: Array) -> Self {
+        PrimitiveType::Array(value)
+    }
+}
