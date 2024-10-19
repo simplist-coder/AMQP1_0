332d81cba60e66e9f9e4994947edc031edeb64fe
Simplist Coder <simplist.coder@gmail.com>
2024-10-19 11:47:14 +0200

    * formatting

--- amqp-utils/src/lib.rs
+++ amqp-utils/src/lib.rs
@@ -1,4 +1,4 @@
-use crate::error::AppError;
+use amqp_error::AppError;
 use std::pin::Pin;
 use tokio_stream::Stream;
 use tokio_stream::StreamExt;
@@ -13,7 +13,7 @@ pub fn verify_bytes_read_eq(actual: usize, expected: usize) -> Result<(), AppErr
 
 /// reads the passed number of bytes from the passed stream.
 /// ensures that exactly the expected number of bytes is read, and returns Err otherwise
-pub(crate) async fn read_bytes(
+pub async fn read_bytes(
     iter: &mut Pin<Box<impl Stream<Item = u8>>>,
     size: usize,
 ) -> Result<Vec<u8>, AppError> {
@@ -30,41 +30,36 @@ pub(crate) async fn read_bytes(
     Ok(res)
 }
 
-pub(crate) async fn read_bytes_2(
+pub async fn read_bytes_2(
     iter: &mut Pin<Box<impl Stream<Item = u8>>>,
 ) -> Result<[u8; 2], AppError> {
     Ok(read_bytes(iter, 2).await?.try_into().unwrap())
 }
 
-pub(crate) async fn read_bytes_4(
+pub async fn read_bytes_4(
     iter: &mut Pin<Box<impl Stream<Item = u8>>>,
 ) -> Result<[u8; 4], AppError> {
     Ok(read_bytes(iter, 4).await?.try_into().unwrap())
 }
 
-pub(crate) async fn read_bytes_8(
+pub async fn read_bytes_8(
     iter: &mut Pin<Box<impl Stream<Item = u8>>>,
 ) -> Result<[u8; 8], AppError> {
     Ok(read_bytes(iter, 8).await?.try_into().unwrap())
 }
 
-pub(crate) async fn read_bytes_16(
+pub async fn read_bytes_16(
     iter: &mut Pin<Box<impl Stream<Item = u8>>>,
 ) -> Result<[u8; 16], AppError> {
     Ok(read_bytes(iter, 16).await?.try_into().unwrap())
 }
 
-#[cfg(test)]
-pub(crate) mod tests {
-    use super::*;
+pub trait ByteVecExt {
+    fn into_pinned_stream(self) -> Pin<Box<impl Stream<Item = u8>>>;
+}
 
-    pub(crate) trait ByteVecExt {
-        fn into_pinned_stream(self) -> Pin<Box<impl Stream<Item = u8>>>;
-    }
-
-    impl ByteVecExt for Vec<u8> {
-        fn into_pinned_stream(self) -> Pin<Box<impl Stream<Item = u8>>> {
-            Box::pin(tokio_stream::iter(self))
-        }
+impl ByteVecExt for Vec<u8> {
+    fn into_pinned_stream(self) -> Pin<Box<impl Stream<Item = u8>>> {
+        Box::pin(tokio_stream::iter(self))
     }
 }
