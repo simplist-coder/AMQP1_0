7b743c1954d0784c5aef43894552ab94884e815d
Simplist Coder <simplist.coder@gmail.com>
2024-10-20 14:10:08 +0200

    * add fields to begin.rs

--- amqp-transport/src/frame/performatives/begin.rs
+++ amqp-transport/src/frame/performatives/begin.rs
@@ -1,9 +1,23 @@
 use amqp_error::AppError;
+use amqp_type::alias::Fields;
 use amqp_type::composite::Composite;
+use amqp_type::primitive::variable_width::symbol::Symbol;
 use std::vec::IntoIter;
 
-#[derive(Debug, Copy, Clone)]
-pub struct Begin {}
+type TransferNumber = u32;
+type Handle = u32;
+
+#[derive(Debug, Clone)]
+pub struct Begin {
+    remote_channel: Option<u16>,
+    next_outgoing_id: TransferNumber,
+    incoming_window: u32,
+    outgoing_window: u32,
+    handle_max: Option<Handle>,
+    offered_capabilities: Vec<Symbol>,
+    desired_capabilities: Vec<Symbol>,
+    properties: Fields,
+}
 
 impl Begin {
     pub fn encode(self) -> Vec<u8> {
