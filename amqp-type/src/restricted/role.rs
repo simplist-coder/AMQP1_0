75c2e58caa91c376a1c8ef19a3399c3f07ebf149
Simplist Coder <simplist.coder@gmail.com>
2024-10-23 22:11:42 +0200

    * implement role.rs

--- amqp-type/src/restricted/role.rs
+++ amqp-type/src/restricted/role.rs
@@ -14,3 +14,32 @@
 
 #[derive(Debug, Clone, Copy)]
 pub struct Role(bool);
+
+impl Role {
+    pub fn new(value: bool) -> Self {
+        Self(value)
+    }
+
+    pub fn is_sender(&self) -> bool {
+        self.0 == false
+    }
+
+    pub fn is_receiver(&self) -> bool {
+        self.0 == true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_role_is_sender_is_true_for_inner_value_false() {
+        assert_eq!(Role::new(false).is_sender(), true);
+    }
+
+    #[test]
+    fn test_role_is_receiver_is_true_for_inner_value_true() {
+        assert_eq!(Role::new(true).is_sender(), true);
+    }
+}
